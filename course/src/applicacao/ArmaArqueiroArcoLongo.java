705c27b31412d94683261bf30d4596ae7840d59c
Daniel Sant'Anna <danielcsantanna3@gmail.com>
2022-10-12 18:46:25 -0300

    novas classes realizadas

---
+++ course/src/applicacao/ArmaArqueiroArcoLongo.java
@@ -0,0 +1,17 @@
+package applicacao;
+
+public class ArmaArqueiroArcoLongo {
+	
+	public double ataque; 
+	public double defesa;
+	
+	public double atacar() {
+		double atacar = this.ataque;
+		return atacar;
+	}
+	
+	public double defender() {
+		double defender = this.defesa;
+		return defender;
+	}
+}
---
+++ course/src/applicacao/ArmaMagoVarinha.java
@@ -0,0 +1,19 @@
+package applicacao;
+
+public class ArmaMagoVarinha {
+	
+	public double ataque; 
+	public double defesa;
+	
+	public double atacar() {
+		double atacar = this.ataque;
+		return atacar;
+	}
+	
+	public double defender() {
+		double defender = this.defesa;
+		return defender;
+	}
+		
+}
+
